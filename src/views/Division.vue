0f3a8cc6c7b6453ae263538f06f509b9434b8228
wolv89 <peter.delafosse@gmail.com>
2024-04-19 00:04:22 +1000

    Matches styling

--- src/views/Division.vue
+++ src/views/Division.vue
@@ -82,3 +82,124 @@ onMounted(() => {
 		<div v-if="content" v-html="content"></div>
 	</div>
 </template>
+<style lang="sass">
+.matches
+	display: flex
+	margin: 0 -2.5rem
+
+	.round
+		position: relative
+		padding: 1rem 0
+		box-sizing: border-box
+		flex-basis: 25%
+		overflow: clip
+		display: flex
+		flex-direction: column
+
+		&-of-1
+			.match-slot
+				flex-basis: 100%
+
+		&-of-2
+			.match-slot
+				flex-basis: 50%
+
+		&-of-4
+			.match-slot
+				flex-basis: 25%
+
+		&-of-8
+			.match-slot
+				flex-basis: 12.5%
+
+		&-of-16
+			.match-slot
+				flex-basis: 6.25%
+
+		&-of-32
+			.match-slot
+				flex-basis: 3.125%
+
+		&-of-64
+			.match-slot
+				flex-basis: 1.5625%
+
+		&-of-128
+			.match-slot
+				flex-basis: 0.78125%
+
+		&:first-child
+			.match
+				&:before
+					display: none
+
+		&:last-child
+			.match
+				&:after,&-slot:after
+					display: none
+
+	.match-slot
+		display: flex
+		align-items: center
+		padding: 1rem 2.5rem
+		position: relative
+		box-sizing: border-box
+
+		&:after
+			content: ''
+			width: 1px
+			height: 50%
+			position: absolute
+			right: 0
+			background: $text
+
+		&:nth-child(odd):after
+			top: 50%
+
+		&:nth-child(even):after
+			bottom: 50%
+
+	.match
+		border: 1px solid #4a4a4a
+		border-radius: 0.2rem
+		background: #fff
+		position: relative
+		flex-basis: 100%
+
+		&:before,&:after
+			content: ''
+			position: absolute
+			top: 50%
+			height: 1px
+			width: 100%
+			background: $text
+
+		&:before
+			right: 100%
+
+		&:after
+			left: 100%
+
+		&-header
+			display: flex
+			justify-content: space-between
+			align-items: center
+			gap: 1rem
+			padding: 0.5rem 0.75rem
+			background: $text
+			color: $background
+
+		&-round
+			+slm(0.8rem, 1rem, 0)
+
+		&-status
+			+slm(0.6rem, 0.9rem, 0)
+
+		&-body
+			padding: 0.75rem
+			display: flex
+			justify-content: space-between
+			align-items: center
+			gap: 0.5rem
+
+</style>
