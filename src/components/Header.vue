58045ddd144d1b9cf873ebf4bee4e72ce35ad6f1
wolv89 <peter.delafosse@gmail.com>
2024-04-19 19:29:46 +1000

    Match updating

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,9 @@
 <script setup>
 
-import { computed } from 'vue'
+import { ref, computed } from 'vue'
 import { store } from '@/store.js'
+
+const showPopNav = ref(false)
 
 const userBtn = computed(() => {
 	if(!store.user) return 'Login'
@@ -21,7 +23,20 @@ const userBtn = computed(() => {
 			</div>
 			<div class="header-slice hs-nav">
 				<nav class="header-nav" v-if="store.event">
-					<h3>{{ store.event.name }}</h3>
+					<button :class="{'icon-btn': true, 'icb-larger': true, 'icb-flip': showPopNav}" @click.prevent="showPopNav = !showPopNav">
+						<span class="icb-icon">
+							<img src="@/assets/images/radix-ui-chev-down.svg" />
+						</span>
+						<span class="icb-text">{{ store.event.name }}</span>
+					</button>
+				</nav>
+				<nav class="pop-nav" v-if="showPopNav">
+					<ol class="pop-nav-links">
+						<li><RouterLink @click="showPopNav = false" :to="'/' + store.event.path">Event Home</RouterLink></li>
+						<li v-for="division in store.divisions" :class="{'is-draft': !division.active}" :key="division.id">
+							<RouterLink @click="showPopNav = false" :to="`/${store.event.path}/${division.path}/`">{{ division.name }}<span v-if="!division.active" class="status">Draft</span></RouterLink>
+						</li>
+					</ol>
 				</nav>
 			</div>
 			<div class="header-slice hs-user">
@@ -54,6 +69,7 @@ const userBtn = computed(() => {
 		.hs-nav
 			flex-basis: 60%
 			text-align: center
+			position: relative
 
 		.hs-user
 			text-align: right
@@ -100,4 +116,42 @@ const userBtn = computed(() => {
 		&:hover
 			img
 				transform: scale(1.2)
+
+.pop-nav
+	position: absolute
+	top: 100%
+	left: calc(50% - 6rem)
+	min-width: 18rem
+	z-index: 150
+	border-radius: calc(0.5rem + 4px)
+	background: $background
+	border: 1px solid $text
+	box-shadow: 0.3rem 0.6rem 0.1rem rgba(0,0,0,0.2)
+	overflow: clip
+
+	ol
+		list-style: none outside
+		margin: 0
+		padding: 0
+
+	a
+		display: block
+		padding: 1rem
+		text-align: left
+		transition: all 0.4s ease
+
+		&:hover
+			background: $compliment
+			color: $background
+
+		&.router-link-exact-active
+			font-weight: bold
+
+		.status
+			display: inline-block
+			text-transform: uppercase
+			font-size: 0.6em
+			padding: 0 0.5rem
+			color: #bbb
+
 </style>
--- src/views/Division.vue
+++ src/views/Division.vue
@@ -1,6 +1,6 @@
 <script setup>
 
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, nextTick, computed, watch } from 'vue'
 import { useGo } from '@/usego'
 import { useRoute } from 'vue-router'
 
@@ -11,6 +11,11 @@ const route = useRoute()
 const division = ref(null)
 const content = ref(null)
 const someError = ref(null)
+
+const pkg = ref(null)
+const editing = ref(0)
+const loadmsg = ref("Loading")
+const errmsg = ref(null)
 
 
 
@@ -57,14 +62,87 @@ async function loadMatches() {
 	const { data, error } = await useGo('/division/' + division.value.id + '/view')
 
 	if(error.value) {
-		someError.value = error.value
 		content.value = null
 	}
 	else {
 		content.value = data.value
+
+		await nextTick()
+
+		bindMatches()
 	}
 
 }
+
+
+function bindMatches() {
+
+	let matchesWrapper = document.getElementById("matches")
+	if(!matchesWrapper) return
+
+	let matchElements = matchesWrapper.getElementsByClassName("match")
+	if(!matchElements) return
+
+	for(let m = 0; m < matchElements.length; m++) {
+		matchElements[m].addEventListener("click", (event) => {
+			openEditor(event.currentTarget.getAttribute("id").replace("match-", ""))
+		})
+	}
+
+}
+
+
+async function openEditor(id) {
+
+	editing.value = 1
+	loadmsg.value = "Loading match"
+
+	const { data, error } = await useGo('/admin/match/' + id).json()
+
+	if(error.value) {
+		errmsg.value = error.value
+		pkg.value = null
+		editing.value = 3
+	}
+	else {
+		pkg.value = data.value
+		editing.value = 2
+	}
+
+}
+
+
+async function updateMatch() {
+
+	editing.value = 1
+	loadmsg.value = "Saving match"
+
+	const submission = {
+		...pkg.value.match
+	}
+
+	submission.status = +submission.status
+	submission.winner = +submission.winner
+
+	const { data, error } = await useGo('/admin/match/' + pkg.value.match.id + '/update', {
+		body: JSON.stringify(submission)
+	}).post().json()
+
+	editing.value = 0
+	pkg.value = null
+	loadMatches()
+
+}
+
+
+
+function closeEditor() {
+
+	pkg.value = null
+	editing.value = 0
+
+}
+
 
 onMounted(() => {
 	if(!store.event) {
@@ -75,131 +153,221 @@ onMounted(() => {
 	}
 })
 
+watch(() => route.params.division, (newDiv, oldDiv) => {
+	division.value = null
+	loadDivision()
+})
+
+
+const e1name = computed(() => {
+
+	if(!pkg.value) return ""
+	if(!pkg.value.entrant1) return ""
+
+	let name = pkg.value.entrant1.p1firstname + " " + pkg.value.entrant1.p1lastname
+
+	if(pkg.value.entrant1.team) {
+		name += " & " + pkg.value.entrant1.p2firstname + " " + pkg.value.entrant1.p2lastname
+	}
+
+	return name
+
+})
+
+
+const e2name = computed(() => {
+
+	if(!pkg.value) return ""
+	if(!pkg.value.entrant2) return ""
+
+	let name = pkg.value.entrant2.p1firstname + " " + pkg.value.entrant2.p1lastname
+
+	if(pkg.value.entrant2.team) {
+		name += " & " + pkg.value.entrant2.p2firstname + " " + pkg.value.entrant2.p2lastname
+	}
+
+	return name
+
+})
+
+
 </script>
 <template>
 	<div class="wrapper wrap-max-lrg">
-		<h2>Division</h2>
-		<div v-if="content" v-html="content"></div>
+		<h2 v-if="division">{{ division.name }}</h2>
+		<p><RouterLink class="edit-link" :to="route.fullPath + 'edit'">Edit division</RouterLink></p>
+		<div :class="{templ: true, 'can-edit': store.user}" v-if="content" v-html="content"></div>
 	</div>
+	<section v-if="editing > 0" class="pop-editor">
+		<div class="pop-content">
+			<article v-if="editing == 1" class="pop-loader">
+				<p>{{ loadmsg }}</p>
+			</article>
+			<article v-else-if="editing == 2" class="pop-struct">
+				<div class="form pop-columns">
+					<div class="pop-column">
+						<h6>Edit Match</h6>
+						<div class="entrants">
+							<div class="entrant1">
+								<p><span class="mini-title">Entrant 1</span>{{ e1name }}</p>
+							</div>
+							<div class="entrants-sep"><span>vs</span></div>
+							<div class="entrant2">
+								<p><span class="mini-title">Entrant 2</span>{{ e2name }}</p>
+							</div>
+						</div>
+					</div>
+					<div class="pop-column">
+						<fieldset class="pop-field">
+							<label class="pop-label">Status</label>
+							<select v-model="pkg.match.status">
+								<option value="0">Pending</option>
+								<option value="1">Ready</option>
+								<option value="2">Playing</option>
+								<option value="3">Finished</option>
+							</select>
+						</fieldset>
+						<fieldset class="pop-field">
+							<label class="pop-label">Winner</label>
+							<select v-model="pkg.match.winner">
+								<option value="0">None</option>
+								<option value="1">Entrant 1</option>
+								<option value="2">Entrant 2</option>
+							</select>
+						</fieldset>
+						<fieldset class="pop-field">
+							<label class="pop-label">Score</label>
+							<input type="text" class="text-input" v-model="pkg.match.score" />
+						</fieldset>
+						<fieldset class="pop-field">
+							<label class="pop-label">Notes</label>
+							<textarea class="text-input" v-model="pkg.match.notes"></textarea>
+						</fieldset>
+						<fieldset class="pop-field">
+							<label class="pop-label">Schedule</label>
+							<input type="text" class="text-input" v-model="pkg.match.start" />
+						</fieldset>
+					</div>
+				</div>
+			</article>
+			<article v-else-if="editing == 3" class="pop-error">
+				<p>{{ errmsg }}</p>
+			</article>
+		</div>
+		<div class="pop-control">
+			<ol class="pop-btns">
+				<li><button @click.prevent="closeEditor" class="btn pop-cancel">Cancel</button></li>
+				<li v-if="editing == 2"><button @click.prevent="updateMatch" class="btn pop-submit">Save</button></li>
+			</ol>
+		</div>
+	</section>
 </template>
 <style lang="sass">
-.matches
+.templ
+	&.can-edit
+		.matches
+			.match
+				cursor: pointer
+
+				&:hover
+					border-color: $theme
+
+					.match
+						&-header,&-sep
+							background: $theme
+
+.pop-editor
+	position: fixed
+	top: 0
+	left: 0
+	width: 100vw
+	height: 100vh
+	overflow: auto
+	z-index: 9999
+	background: rgba(255,255,255,0.8)
+	backdrop-filter: blur(5px)
 	display: flex
-	margin: 0 -2.5rem
+	flex-flow: column
+	justify-content: center
+	align-items: center
+	gap: 1rem
 
-	.round
-		position: relative
-		padding: 1rem 0
-		box-sizing: border-box
-		flex-basis: 25%
-		overflow: clip
-		display: flex
-		flex-direction: column
+	.pop
+		&-content
+			box-sizing: border-box
+			flex-basis: 55%
+			padding: 2rem 1rem 0
 
-		&-of-1
-			.match-slot
-				flex-basis: 100%
+			.entrants
+				background: $hover
+				padding: 1rem
 
-		&-of-2
-			.match-slot
-				flex-basis: 50%
+				.mini-title
+					display: block
+					+slm(0.6rem, 0.8rem, 0)
+					text-transform: uppercase
+					padding: 0.25rem 0
+					opacity: 0.3
 
-		&-of-4
-			.match-slot
-				flex-basis: 25%
+				&-sep
+					position: relative
+					text-align: center
+					padding: 0.25rem 0
+					+slm(0.6rem, 0.8rem, 0)
 
-		&-of-8
-			.match-slot
-				flex-basis: 12.5%
+					&:before,&:after
+						content: ''
+						position: absolute
+						width: 40%
+						height: 1px
+						background: $border
+						top: 50%
 
-		&-of-16
-			.match-slot
-				flex-basis: 6.25%
+					&:before
+						left: 0
 
-		&-of-32
-			.match-slot
-				flex-basis: 3.125%
+					&:after
+						right: 0
 
-		&-of-64
-			.match-slot
-				flex-basis: 1.5625%
+					span
+						position: relative
+						z-index: 10
+						display: inline-block
+						text-transform: uppercase
 
-		&-of-128
-			.match-slot
-				flex-basis: 0.78125%
-
-		&:first-child
-			.match
-				&:before
-					display: none
-
-		&:last-child
-			.match
-				&:after,&-slot:after
-					display: none
-
-	.match-slot
-		display: flex
-		align-items: center
-		padding: 1rem 2.5rem
-		position: relative
-		box-sizing: border-box
-
-		&:after
-			content: ''
-			width: 1px
-			height: 50%
-			position: absolute
-			right: 0
-			background: $text
-
-		&:nth-child(odd):after
-			top: 50%
-
-		&:nth-child(even):after
-			bottom: 50%
-
-	.match
-		border: 1px solid #4a4a4a
-		border-radius: 0.2rem
-		background: #fff
-		position: relative
-		flex-basis: 100%
-
-		&:before,&:after
-			content: ''
-			position: absolute
-			top: 50%
-			height: 1px
-			width: 100%
-			background: $text
-
-		&:before
-			right: 100%
-
-		&:after
-			left: 100%
-
-		&-header
+		&-columns
 			display: flex
 			justify-content: space-between
-			align-items: center
 			gap: 1rem
-			padding: 0.5rem 0.75rem
-			background: $text
-			color: $background
 
-		&-round
-			+slm(0.8rem, 1rem, 0)
+			.pop-column
+				flex-basis: 45%
 
-		&-status
-			+slm(0.6rem, 0.9rem, 0)
+		&-control
+			flex-basis: 25%
 
-		&-body
-			padding: 0.75rem
+		&-btns
 			display: flex
-			justify-content: space-between
-			align-items: center
-			gap: 0.5rem
+			gap: 1rem
+			justify-content: center
+			list-style: none outside
+			margin: 0
+			padding: 0
+
+			li
+				padding: 0.5rem
+
+			.btn
+				padding-left: 1.5rem
+				padding-right: 1.5rem
+
+				&.pop-cancel
+					background: $half
+
+				&.pop-submit
+					background: $theme
+
+				&:hover
+					background: $text
 
 </style>
